99dbbd244641badfadca73e8abee8e2a54952bbf
ChromeMatic <armanibrwn@gmail.com>
2024-11-14 16:40:43 -0500

    Initial setup and pages added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+// @ts-ignore
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@vueuse/motion/nuxt',
+    ['@pinia/nuxt',{ autoImports: ['defineStore'] },],
+    '@nuxtjs/google-fonts',
+  ]
 })
---
+++ pages/Sevices.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <section class="w-full h-screen flex justify-center items-center bg-green-100">
+    <h1 class="text-3xl overline">
+      Code-Works Services Page
+    </h1>
+  </section>
+</template>
